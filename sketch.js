a2d63819c1dedb0563fa9b452ee7794b0274d740
PlayfulPawsProd <PlayfulPawsProductions@gmail.com>
2025-03-29 19:33:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 // PART 1 of 2 - Integrated Basic Gacha Screen! Nya!
 
 // --- Version ---
-const gameVersion = "v1.12c"; // This MUST match the version you intend to run
+const gameVersion = "v1.12f"; // This MUST match the version you intend to run
 
 let kitty;
 let plushies = [];
