f8b226bef6ee587f9190fed262f23a41a3cf30a7
PlayfulPawsProd <PlayfulPawsProductions@gmail.com>
2025-03-31 00:17:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 // PART 1 of 2 - Smoother Background Transitions! Nya!
 
 // --- Version ---
-const gameVersion = "v1.14q"; // This MUST match the version you intend to run
+const gameVersion = "v1.15"; // This MUST match the version you intend to run
 
 let kitty;
 let plushies = [];
